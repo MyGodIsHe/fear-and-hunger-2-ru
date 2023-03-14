2dbd6d8d7e548f2b651afb169e69deeed10300e1
Ilya Chistyakov <ilchistyakov@gmail.com>
2023-03-14 14:27:21 +0600

    new doc

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 API_SERVICE_NAME = 'drive'
 API_VERSION = 'v3'
 DOC_IDS = [
-    '1It-ptFBxDCnCsYWFyqCBY-HlOXVxOjen7lI2WZtdT9k',
+    '1al4YzWZT2G6QUVbcVUHa_qbT41IoK6i8RKiyt0ItQS8',
     '1hJd5UT-3dom3jHQaDyBo47TnQpWvPAGSvHcJaPuGBLk',
 ]
